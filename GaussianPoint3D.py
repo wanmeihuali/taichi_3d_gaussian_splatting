b99ac2f69b9be5202f7e91a979ea9b321b1a2e81
wanmeihuali <kuangyuansun@gmail.com>
2023-05-04 21:00:23 -0700

    test jacobian working.

---
+++ GaussianPoint3D.py
@@ -0,0 +1,241 @@
+# %%
+import taichi as ti
+import taichi.math
+
+mat2x3f = ti.types.matrix(n=2, m=3, dtype=ti.f32)
+mat9x9f = ti.types.matrix(n=9, m=9, dtype=ti.f32)
+mat9x3f = ti.types.matrix(n=9, m=3, dtype=ti.f32)
+mat4x9f = ti.types.matrix(n=4, m=9, dtype=ti.f32)
+mat9x4f = ti.types.matrix(n=9, m=4, dtype=ti.f32)
+
+@ti.func
+def rotation_matrix_from_quaternion(q: ti.math.vec4) -> ti.math.mat3:
+    """
+    Convert a quaternion to a rotation matrix.
+    """
+    xx = q.x * q.x
+    yy = q.y * q.y
+    zz = q.z * q.z
+    xy = q.x * q.y
+    xz = q.x * q.z
+    yz = q.y * q.z
+    wx = q.w * q.x
+    wy = q.w * q.y
+    wz = q.w * q.z
+    return ti.math.mat3([
+        [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy)],
+        [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx)],
+        [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy)]
+    ])
+    
+@ti.func
+def tranform_matrix_from_quaternion_and_translation(q: ti.math.vec4, t: ti.math.vec3) -> ti.math.mat4:
+    """
+    Convert a quaternion and a translation to a transformation matrix.
+    """
+    R = rotation_matrix_from_quaternion(q)
+    return ti.math.mat4([
+        [R[0, 0], R[0, 1], R[0, 2], t[0]],
+        [R[1, 0], R[1, 1], R[1, 2], t[1]],
+        [R[2, 0], R[2, 1], R[2, 2], t[2]],
+        [0, 0, 0, 1]
+    ])
+
+
+@ti.dataclass
+class GaussianPoint3D:
+    """
+    As the paper said: The covariance matrix Σ of a 3D Gaussian is analogous to describing the configuration of an ellipsoid. 
+    """
+    cov_rotation: ti.math.vec4 # quaternion of cov_rotation, x, y, z, w
+    cov_scale: ti.math.vec3 # cov_scale of x, y, z
+    translation: ti.math.vec3 # translation of x, y, z
+    alpha: ti.f32 # opacity of the point
+    
+
+    @ti.func
+    def project_to_camera(
+        self,
+        T_camera_world: ti.math.mat4,
+        projective_transform: ti.math.mat3,
+        projective_transform_jacobian: mat2x3f
+    ):
+        """
+        Project the Gaussian point to camera space, without jacobian.
+        """
+        J = projective_transform_jacobian
+        T = T_camera_world
+        R = rotation_matrix_from_quaternion(self.cov_rotation)
+        S = ti.math.mat3([
+            [self.cov_scale.x, 0, 0],
+            [0, self.cov_scale.y, 0],
+            [0, 0, self.cov_scale.z]
+        ])
+        Sigma = R @ S @ S.transpose() @ R.transpose() # covariance matrix, 3x3, equation (6) in the paper
+
+        homogeneous_translation_camera = T_camera_world @ ti.math.vec4(self.translation.x, self.translation.y, self.translation.z, 1)
+        translation_camera = ti.math.vec3(homogeneous_translation_camera.x, homogeneous_translation_camera.y, homogeneous_translation_camera.z)
+        uv1 = (projective_transform @ translation_camera) / translation_camera.z
+        uv = ti.math.vec2(uv1.x, uv1.y)
+        
+        W = ti.math.mat3([
+           [T[0, 0], T[0, 1], T[0, 2]],
+           [T[1, 0], T[1, 1], T[1, 2]],
+           [T[2, 0], T[2, 1], T[2, 2]]
+        ])
+
+        cov_uv = J @ W @ Sigma @ W.transpose() @ J.transpose() # equation (5) in the paper
+        return uv, cov_uv
+    
+    @ti.func
+    def project_to_camera_with_jacobian(
+        self,
+        T_camera_world: ti.math.mat4,
+        projective_transform: ti.math.mat3,
+        projective_transform_jacobian: mat2x3f
+    ):
+        """
+        Project the Gaussian point to camera space, with jacobian.
+        """
+        J = projective_transform_jacobian
+        T = T_camera_world
+        R = rotation_matrix_from_quaternion(self.cov_rotation)
+        S = ti.math.mat3([
+            [self.cov_scale.x, 0, 0],
+            [0, self.cov_scale.y, 0],
+            [0, 0, self.cov_scale.z]
+        ])
+        M = R @ S
+        Sigma = M @ M.transpose() # covariance matrix, 3x3, equation (6) in the paper
+
+        homogeneous_translation_camera = T_camera_world @ ti.math.vec4(self.translation.x, self.translation.y, self.translation.z, 1)
+        translation_camera = ti.math.vec3(homogeneous_translation_camera.x, homogeneous_translation_camera.y, homogeneous_translation_camera.z)
+        uv1 = (projective_transform @ translation_camera) / translation_camera.z
+        uv = ti.math.vec2(uv1.x, uv1.y)
+        
+        W = ti.math.mat3([
+           [T[0, 0], T[0, 1], T[0, 2]],
+           [T[1, 0], T[1, 1], T[1, 2]],
+           [T[2, 0], T[2, 1], T[2, 2]]
+        ])
+
+        U = J @ W # 2x3
+        cov_uv = U @ Sigma @ U.transpose() # equation (5) in the paper
+        
+        # the forward output is uv, cov_uv, the backward input is d_uv, d_cov_uv
+        # jacobian: d_uv / d_translation, d_cov_uv / d_cov_rotation, d_cov_uv / d_cov_scale
+        # in paper, d_cov_uv / d_cov_rotation is called d_Sigma' / dq, d_cov_uv / d_cov_scale is called d_Sigma' / ds
+    
+        t = self.translation
+        K = projective_transform
+        #d_uv_d_translation_camera = \left[\begin{matrix}\frac{K_{0, 0}}{t_{2, 0}} & \frac{K_{0, 1}}{t_{2, 0}} & \frac{- K_{0, 0} t_{0, 0} - K_{0, 1} t_{1, 0}}{t_{2, 0}^{2}}\\\frac{K_{1, 0}}{t_{2, 0}} & \frac{K_{1, 1}}{t_{2, 0}} & \frac{- K_{1, 0} t_{0, 0} - K_{1, 1} t_{1, 0}}{t_{2, 0}^{2}}\end{matrix}\right]
+        d_uv_d_translation_camera = mat2x3f([
+            [K[0, 0] / t.z, K[0, 1] / t.z, (-K[0, 0] * t.x - K[0, 1] * t.y) / (t.z * t.z)],
+            [K[1, 0] / t.z, K[1, 1] / t.z, (-K[1, 0] * t.x - K[1, 1] * t.y) / (t.z * t.z)]])
+        d_translation_camera_d_translation = W
+        d_uv_d_translation = d_uv_d_translation_camera @ d_translation_camera_d_translation # 2 x 3
+
+        # Sigma' = U @ Sigma @ U^T, so
+        # d_Sigma' / d_Sigma = 
+        # \left[\begin{matrix}U_{0, 0}^{2} & U_{0, 0} U_{0, 1} & U_{0, 0} U_{0, 2} & U_{0, 0} U_{0, 1} & U_{0, 1}^{2} & U_{0, 1} U_{0, 2} & U_{0, 0} U_{0, 2} & U_{0, 1} U_{0, 2} & U_{0, 2}^{2}\\U_{0, 0} U_{1, 0} & U_{0, 0} U_{1, 1} & U_{0, 0} U_{1, 2} & U_{0, 1} U_{1, 0} & U_{0, 1} U_{1, 1} & U_{0, 1} U_{1, 2} & U_{0, 2} U_{1, 0} & U_{0, 2} U_{1, 1} & U_{0, 2} U_{1, 2}\\U_{0, 0} U_{1, 0} & U_{0, 1} U_{1, 0} & U_{0, 2} U_{1, 0} & U_{0, 0} U_{1, 1} & U_{0, 1} U_{1, 1} & U_{0, 2} U_{1, 1} & U_{0, 0} U_{1, 2} & U_{0, 1} U_{1, 2} & U_{0, 2} U_{1, 2}\\U_{1, 0}^{2} & U_{1, 0} U_{1, 1} & U_{1, 0} U_{1, 2} & U_{1, 0} U_{1, 1} & U_{1, 1}^{2} & U_{1, 1} U_{1, 2} & U_{1, 0} U_{1, 2} & U_{1, 1} U_{1, 2} & U_{1, 2}^{2}\end{matrix}\right]
+        # as Sigma' and Sigma are matrix and the derivative is a tensor, so we use a 2x2x3x3 tensor to represent the derivative
+        # However, taichi does not support tensor well, so we flatten the tensor to a matrix 4x9
+        d_Sigma_prime_d_Sigma = mat4x9f([
+            [U[0, 0] * U[0, 0], U[0, 0] * U[0, 1], U[0, 0] * U[0, 2], U[0, 0] * U[0, 1], U[0, 1] * U[0, 1], U[0, 1] * U[0, 2], U[0, 0] * U[0, 2], U[0, 1] * U[0, 2], U[0, 2] * U[0, 2]],
+            [U[0, 0] * U[1, 0], U[0, 0] * U[1, 1], U[0, 0] * U[1, 2], U[0, 1] * U[1, 0], U[0, 1] * U[1, 1], U[0, 1] * U[1, 2], U[0, 2] * U[1, 0], U[0, 2] * U[1, 1], U[0, 2] * U[1, 2]],
+            [U[0, 0] * U[1, 0], U[0, 1] * U[1, 0], U[0, 2] * U[1, 0], U[0, 0] * U[1, 1], U[0, 1] * U[1, 1], U[0, 2] * U[1, 1], U[0, 0] * U[1, 2], U[0, 1] * U[1, 2], U[0, 2] * U[1, 2]],
+            [U[1, 0] * U[1, 0], U[1, 0] * U[1, 1], U[1, 0] * U[1, 2], U[1, 0] * U[1, 1], U[1, 1] * U[1, 1], U[1, 1] * U[1, 2], U[1, 0] * U[1, 2], U[1, 1] * U[1, 2], U[1, 2] * U[1, 2]]
+        ])
+        # d_Sigma_d_M is 3x3x3x3 tensor, we flatten it to a matrix 9x9
+        # \left[\begin{matrix}2 m_{00} & 2 m_{01} & 2 m_{02} & 0 & 0 & 0 & 0 & 0 & 0\\m_{10} & m_{11} & m_{12} & m_{00} & m_{01} & m_{02} & 0 & 0 & 0\\m_{20} & m_{21} & m_{22} & 0 & 0 & 0 & m_{00} & m_{01} & m_{02}\\m_{10} & m_{11} & m_{12} & m_{00} & m_{01} & m_{02} & 0 & 0 & 0\\0 & 0 & 0 & 2 m_{10} & 2 m_{11} & 2 m_{12} & 0 & 0 & 0\\0 & 0 & 0 & m_{20} & m_{21} & m_{22} & m_{10} & m_{11} & m_{12}\\m_{20} & m_{21} & m_{22} & 0 & 0 & 0 & m_{00} & m_{01} & m_{02}\\0 & 0 & 0 & m_{20} & m_{21} & m_{22} & m_{10} & m_{11} & m_{12}\\0 & 0 & 0 & 0 & 0 & 0 & 2 m_{20} & 2 m_{21} & 2 m_{22}\end{matrix}\right]
+        d_Sigma_d_M = mat9x9f([
+            [2 * M[0, 0], 2 * M[0, 1], 2 * M[0, 2], 0, 0, 0, 0, 0, 0],
+            [M[1, 0], M[1, 1], M[1, 2], M[0, 0], M[0, 1], M[0, 2], 0, 0, 0],
+            [M[2, 0], M[2, 1], M[2, 2], 0, 0, 0, M[0, 0], M[0, 1], M[0, 2]],
+            [M[1, 0], M[1, 1], M[1, 2], M[0, 0], M[0, 1], M[0, 2], 0, 0, 0],
+            [0, 0, 0, 2 * M[1, 0], 2 * M[1, 1], 2 * M[1, 2], 0, 0, 0],
+            [0, 0, 0, M[2, 0], M[2, 1], M[2, 2], M[1, 0], M[1, 1], M[1, 2]],
+            [M[2, 0], M[2, 1], M[2, 2], 0, 0, 0, M[0, 0], M[0, 1], M[0, 2]],
+            [0, 0, 0, M[2, 0], M[2, 1], M[2, 2], M[1, 0], M[1, 1], M[1, 2]],
+            [0, 0, 0, 0, 0, 0, 2 * M[2, 0], 2 * M[2, 1], 2 * M[2, 2]]
+        ])
+        
+        d_Sigma_prime_d_M = d_Sigma_prime_d_Sigma @ d_Sigma_d_M # 4x9
+        # M = R @ S, so d_M / d_S = R
+        # \left[\begin{matrix}R_{0, 0} & 0 & 0\\0 & R_{0, 1} & 0\\0 & 0 & R_{0, 2}\\R_{1, 0} & 0 & 0\\0 & R_{1, 1} & 0\\0 & 0 & R_{1, 2}\\R_{2, 0} & 0 & 0\\0 & R_{2, 1} & 0\\0 & 0 & R_{2, 2}\end{matrix}\right]
+        d_M_d_s = mat9x3f([
+            [R[0, 0], 0, 0],
+            [0, R[0, 1], 0],
+            [0, 0, R[0, 2]],
+            [R[1, 0], 0, 0],
+            [0, R[1, 1], 0],
+            [0, 0, R[1, 2]],
+            [R[2, 0], 0, 0],
+            [0, R[2, 1], 0],
+            [0, 0, R[2, 2]],
+        ])
+        d_Sigma_prime_d_s = d_Sigma_prime_d_M @ d_M_d_s # 4x3
+        
+        # d_M / dq is 4x3x3 tensor, we flatten it to a matrix 9 x 4
+        # \left[\begin{matrix}0 & - 4 s_{00} y & - 4 s_{00} z & 0\\2 s_{11} y & 2 s_{11} x & - 2 s_{11} w & - 2 s_{11} z\\2 s_{22} z & 2 s_{22} w & 2 s_{22} x & 2 s_{22} y\\2 s_{00} y & 2 s_{00} x & 2 s_{00} w & 2 s_{00} z\\- 4 s_{11} x & 0 & - 4 s_{11} z & 0\\- 2 s_{22} w & 2 s_{22} z & 2 s_{22} y & - 2 s_{22} x\\2 s_{00} z & - 2 s_{00} w & 2 s_{00} x & - 2 s_{00} y\\2 s_{11} w & 2 s_{11} z & 2 s_{11} y & 2 s_{11} x\\- 4 s_{22} x & - 4 s_{22} y & 0 & 0\end{matrix}\right]
+        s = self.cov_scale
+        q = self.cov_rotation
+        d_M_d_q = mat9x4f([
+            [0, -4 * s.x * q.y, -4 * s.x * q.z, 0],
+            [2 * s.y * q.y, 2 * s.y * q.x, -2 * s.y * q.w, -2 * s.y * q.z],
+            [2 * s.z * q.z, 2 * s.z * q.w, 2 * s.z * q.x, 2 * s.z * q.y],
+            [2 * s.x * q.y, 2 * s.x * q.x, 2 * s.x * q.w, 2 * s.x * q.z],
+            [-4 * s.y * q.x, 0, -4 * s.y * q.z, 0],
+            [-2 * s.z * q.w, 2 * s.z * q.z, 2 * s.z * q.y, -2 * s.z * q.x],
+            [2 * s.x * q.z, -2 * s.x * q.w, 2 * s.x * q.x, -2 * s.x * q.y],
+            [2 * s.y * q.w, 2 * s.y * q.z, 2 * s.y * q.y, 2 * s.y * q.x],
+            [-4 * s.z * q.x, -4 * s.z * q.y, 0, 0]
+        ])
+        d_Sigma_prime_d_q = d_Sigma_prime_d_M @ d_M_d_q  # 4x4
+        return uv, cov_uv, d_uv_d_translation, d_Sigma_prime_d_q, d_Sigma_prime_d_s
+
+        
+# %%
+ti.init(ti.cpu)
+
+a = GaussianPoint3D.field(shape=())
+@ti.kernel
+def test():
+    a[None] = GaussianPoint3D(
+        cov_rotation=ti.math.vec4(0, 0, 0, 1), 
+        cov_scale=ti.math.vec3(1, 2, 3),
+        translation=ti.math.vec3(0, 1, 1))
+
+    j = mat2x3f([[1, 0, 1], [0, 1, 1]])
+    t = ti.math.mat4([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0 ,1, 0], [0, 0, 0, 1]])
+    p = ti.math.mat3([[2, 0, 0], [0, 2, 0], [0, 0, 1]])
+
+    uv, cov_uv, d_uv_d_translation, d_Sigma_prime_d_q, d_Sigma_prime_d_s = a[None].project_to_camera_with_jacobian(t, p, j)
+    print(uv)
+    print(cov_uv)
+    print(d_uv_d_translation)
+    print(d_Sigma_prime_d_q)
+    print(d_Sigma_prime_d_s)
+    a[None] = GaussianPoint3D(
+        cov_rotation=ti.math.vec4(0.00, 0.00, 0.0, 1.1), 
+        cov_scale=ti.math.vec3(1.00, 2.00, 3.00),
+        translation=ti.math.vec3(0.00, 1, 1))
+    a[None].cov_rotation = ti.math.normalize(a[None].cov_rotation)
+
+
+    j = mat2x3f([[1, 0, 1], [0, 1, 1]])
+    t = ti.math.mat4([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0 ,1, 0], [0, 0, 0, 1]])
+    p = ti.math.mat3([[2, 0, 0], [0, 2, 0], [0, 0, 1]])
+
+    uv, cov_uv, d_uv_d_translation, d_Sigma_prime_d_q, d_Sigma_prime_d_s = a[None].project_to_camera_with_jacobian(t, p, j)
+    print(uv)
+    print(cov_uv)
+    print(d_uv_d_translation)
+    print(d_Sigma_prime_d_q)
+    print(d_Sigma_prime_d_s)
+test()
+
+    
+# %%
+
+
---
+++ playground.py
@@ -0,0 +1,219 @@
+# %%
+import sympy
+from sympy import latex, pprint
+# %%
+"""
+J = projective_transform_jacobian
+T = T_camera_world
+R = rotation_matrix_from_quaternion(self.cov_rotation)
+S = self.cov_scale
+Sigma = R @ S.outer_product(S) @ R.transpose()
+
+homogeneous_translation_camera = T_camera_world @ ti.math.vec4(self.translation.x, self.translation.y, self.translation.z, 1)
+translation_camera = ti.math.vec3(homogeneous_translation_camera.x, homogeneous_translation_camera.y, homogeneous_translation_camera.z)
+uv1 = (projective_transform @ translation_camera) / translation_camera.z
+uv = ti.math.vec2(uv1.x, uv1.y)
+"""
+"""
+T is 4x4
+projective_transform is 3x3
+get Jacobian of D(uv)/D(translation)
+"""
+# %%
+projective_transform = sympy.MatrixSymbol('K', 3, 3)
+translation_camera = sympy.MatrixSymbol('t', 3, 1)
+uv1 = (projective_transform @ translation_camera) / translation_camera[2, 0]
+uv = sympy.Matrix([uv1[0, 0], uv1[1, 0]])
+D_uv_D_translation_camera = uv.jacobian(translation_camera)
+D_uv_D_translation_camera.simplify()
+print(latex(D_uv_D_translation_camera))
+pprint(D_uv_D_translation_camera, use_unicode=True)
+# %%
+T = sympy.MatrixSymbol('T', 4, 4)
+translation = sympy.MatrixSymbol('t', 3, 1)
+homogeneous_translation_camera = T @ sympy.Matrix([translation[0, 0], translation[1, 0], translation[2, 0], 1])
+translation_camera = sympy.Matrix([homogeneous_translation_camera[0, 0], homogeneous_translation_camera[1, 0], homogeneous_translation_camera[2, 0]])
+
+D_translation_camrea_D_translation = translation_camera.jacobian(translation)
+D_translation_camrea_D_translation.simplify()
+print(latex(D_translation_camrea_D_translation))
+pprint(D_translation_camrea_D_translation, use_unicode=True)
+
+# %%
+
+uv1 = (projective_transform @ translation_camera) / translation_camera[2, 0]
+uv = sympy.Matrix([uv1[0, 0], uv1[1, 0]])
+
+
+
+
+D_uv_D_translation = uv.jacobian(translation)
+D_uv_D_translation.simplify()
+print(latex(D_uv_D_translation))
+pprint(D_uv_D_translation, use_unicode=True)
+# %%
+"""
+U = J @ W # 2x3
+cov_uv = U @ Sigma @ U.transpose() # equation (5) in the paper
+"""
+U = sympy.MatrixSymbol('U', 2, 3)
+Sigma = sympy.MatrixSymbol('Sigma', 3, 3)
+Sigma_vec = sympy.Matrix([[Sigma[i, j]] for i in range(3) for j in range(3)])
+cov_uv = U @ Sigma @ U.transpose()
+cov_uv_vec = sympy.Matrix([[cov_uv[i, j]] for i in range(2) for j in range(2)])
+D_cov_uv_D_U = cov_uv_vec.jacobian(Sigma_vec)
+print(D_cov_uv_D_U.shape)
+print(latex(D_cov_uv_D_U))
+pprint(D_cov_uv_D_U, use_unicode=True)
+
+
+# %%
+"""
+Sigma = M @ M.transpose() # covariance matrix, 3x3, equation (6) in the paper
+"""
+"""
+M = sympy.MatrixSymbol('M', 3, 3)
+Sigma = M @ M.transpose()
+D_Sigma_D_M = Sigma.diff(M).as_explicit()
+print(D_Sigma_D_M.shape)
+print(latex(D_Sigma_D_M))
+pprint(D_Sigma_D_M, use_unicode=True)
+"""
+M = sympy.Matrix([[sympy.Symbol(f"m{i}{j}") for j in range(3)] for i in range(3)])
+M_vector = sympy.Matrix([[M[i, j]] for i in range(3) for j in range(3)])
+
+Sigma = M @ M.transpose()
+Sigma_vector = sympy.Matrix([[Sigma[i, j]] for i in range(3) for j in range(3)])
+D_Sigma_D_M = Sigma_vector.jacobian(M_vector)
+print(D_Sigma_D_M.shape)
+print(latex(D_Sigma_D_M))
+pprint(D_Sigma_D_M, use_unicode=True)
+# %%
+# M = R @ S
+R = sympy.MatrixSymbol('R', 3, 3)
+S = sympy.Matrix([
+    [sympy.Symbol("s00"), 0, 0],  
+    [0, sympy.Symbol("s11"), 0],
+    [0, 0, sympy.Symbol("s22")]
+])
+s_vec = sympy.Matrix([S[i, i] for i in range(3)])
+M = R @ S
+M_vector = sympy.Matrix([[M[i, j]] for i in range(3) for j in range(3)])
+D_M_D_s = M_vector.jacobian(s_vec)
+print(D_M_D_s.shape)
+print(latex(D_M_D_s))
+pprint(D_M_D_s, use_unicode=True)
+# %%
+"""
+
+@ti.func
+def rotation_matrix_from_quaternion(q: ti.math.vec4) -> ti.math.mat3:
+    xx = q.x * q.x
+    yy = q.y * q.y
+    zz = q.z * q.z
+    xy = q.x * q.y
+    xz = q.x * q.z
+    yz = q.y * q.z
+    wx = q.w * q.x
+    wy = q.w * q.y
+    wz = q.w * q.z
+    return ti.math.mat3([
+        [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy)],
+        [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx)],
+        [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy)]
+    ])
+ 
+"""
+x = sympy.Symbol('x')
+y = sympy.Symbol('y')
+z = sympy.Symbol('z')
+w = sympy.Symbol('w')
+xx = x * x
+yy = y * y
+zz = z * z
+xy = x * y
+xz = x * z
+yz = y * z
+wx = w * x
+wy = w * y
+wz = w * z
+q = sympy.Matrix([[x], [y], [z], [w]])
+R = sympy.Matrix([
+    [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy)],
+    [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx)],
+    [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy)]
+])
+S = sympy.Matrix([
+    [sympy.Symbol("s00"), 0, 0],  
+    [0, sympy.Symbol("s11"), 0],
+    [0, 0, sympy.Symbol("s22")]
+])
+M = R @ S
+M_vector = sympy.Matrix([[M[i, j]] for i in range(3) for j in range(3)])
+d_M_d_q = M_vector.jacobian(q)
+print(d_M_d_q.shape)
+print(latex(d_M_d_q))
+pprint(d_M_d_q, use_unicode=True)
+
+# %%
+R = sympy.MatrixSymbol('R', 3, 3)
+S = sympy.Matrix([
+    [sympy.Symbol("s00"), 0, 0],  
+    [0, sympy.Symbol("s11"), 0],
+    [0, 0, sympy.Symbol("s22")]
+])
+Sigma = R @ S @ S.transpose() @ R.transpose()
+pprint(Sigma.as_explicit(), use_unicode=True)
+# %%
+R = sympy.MatrixSymbol('R', 3, 3)
+S = sympy.MatrixSymbol('S', 3, 1)
+Sigma = R @ S @ S.transpose() @ R.transpose()
+pprint(Sigma.as_explicit(), use_unicode=True)
+#
+# %%
+import sympy
+from sympy import latex, pprint
+J = sympy.MatrixSymbol('J', 2, 3)
+T = sympy.MatrixSymbol('T', 4, 4)
+cx = sympy.Symbol('cx')
+cy = sympy.Symbol('cy')
+cz = sympy.Symbol('cz')
+cw = sympy.Symbol('cw')
+
+cov = sympy.Matrix([cx, cy, cz, cw])
+R = sympy.Matrix([
+    [1 - 2 * (cy * cy + cz * cz), 2 * (cx * cy - cw * cz), 2 * (cx * cz + cw * cy)],
+    [2 * (cx * cy + cw * cz), 1 - 2 * (cx * cx + cz * cz), 2 * (cy * cz - cw * cx)],
+    [2 * (cx * cz - cw * cy), 2 * (cy * cz + cw * cx), 1 - 2 * (cx * cx + cy * cy)]
+])
+tx = sympy.Symbol('tx')
+ty = sympy.Symbol('ty')
+tz = sympy.Symbol('tz')
+translation = sympy.Matrix([tx, ty, tz])
+sx = sympy.Symbol('sx')
+sy = sympy.Symbol('sy')
+sz = sympy.Symbol('sz')
+s = sympy.Matrix([sx, sy, sz])
+S = sympy.Matrix([
+    [s[0, 0], 0, 0],  
+    [0, s[1, 0], 0],
+    [0, 0, s[2, 0]]
+])
+Sigma = R @ S @ S.transpose() @ R.transpose()
+homogeneous_translation_camera = T @ sympy.Matrix([translation[0, 0], translation[1, 0], translation[2, 0], 1])
+translation_camera = sympy.Matrix([homogeneous_translation_camera[0, 0], homogeneous_translation_camera[1, 0], homogeneous_translation_camera[2, 0]])
+uv1 = (J @ translation_camera) / translation_camera[2, 0]
+uv = sympy.Matrix([uv1[0, 0], uv1[1, 0]])
+W = T[:3, :3]
+cov_uv = J @ W @ Sigma @ W.transpose() @ J.transpose()
+cov_uv_vec = sympy.Matrix([cov_uv[0, 0], cov_uv[0, 1], cov_uv[1, 0], cov_uv[1, 1]])
+J_cov_uv_cov = cov_uv_vec.jacobian(cov)
+print(J_cov_uv_cov.shape)
+print(sympy.python(J_cov_uv_cov))
+J_cov_uv_s = cov_uv_vec.jacobian(s)
+print(J_cov_uv_s.shape)
+print(sympy.python(J_cov_uv_s))
+J_uv_translation = uv.jacobian(translation)
+print(J_uv_translation.shape)
+print(sympy.python(J_uv_translation))
+# %%
