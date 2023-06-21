3cefc23694a6368b68640854f2b8575752b87623
wanmeihuali <kuangyuansun@gmail.com>
2023-06-20 19:45:37 -0700

    add commit premission

--- ci/run_experiment.py
+++ ci/run_experiment.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     git_repo = os.environ["GITHUB_REPO"]
     git_sha = os.environ["GITHUB_SHA"]
     github_client = Github(github_access_token)
-    print(f"Getting pull request {pull_request_number} from {git_owner}/{git_repo}")
+    print(f"Getting pull request {pull_request_number} from {git_repo}")
     github_repo = github_client.get_repo(f"{git_repo}")
     pull_request = github_repo.get_pull(int(pull_request_number))
     pull_request.create_issue_comment(f"Running experiment on sagemaker with git sha {git_sha}")
