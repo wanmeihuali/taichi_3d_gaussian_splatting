cd38d5bbe96c3a4049dd46ea136494d1454aa4b4
wanmeihuali <kuangyuansun@gmail.com>
2023-06-21 00:06:10 -0700

    list some more things

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -5,4 +5,6 @@ if [ -z "$TRAIN_CONFIG" ]; then
     echo "TRAIN_CONFIG is not set"
     exit 1
 fi
+ls /opt/ml/input/data
+ls /opt/ml/input/data/training
 python3 gaussian_point_train.py --train_config $TRAIN_CONFIG
