021e9d680f88d495476fcbd81637fdafcb1a764f
wanmeihuali <kuangyuansun@gmail.com>
2023-10-05 18:18:04 -0700

    Update CameraPoses.py

--- taichi_3d_gaussian_splatting/CameraPoses.py
+++ taichi_3d_gaussian_splatting/CameraPoses.py
@@ -15,17 +15,6 @@ class CameraPoses(nn.Module):
             assert column in self.df.columns, f"column {column} is not in the dataset"
 
         q_pointcloud_camera_table, t_pointcloud_camera_table, self.num_objects = self._get_all_camera_poses()
-        """
-        q_camera_pointcloud, t_camera_pointcloud = inverse_SE3_qt_torch(
-                    q=q_pointcloud_camera, t=t_pointcloud_camera)
-        """
-        """
-        self.input_q_pointcloud_camera_table = q_pointcloud_camera_table
-        self.input_t_pointcloud_camera_table = t_pointcloud_camera_table
-        
-        self.q_pointcloud_camera_table = nn.Parameter(q_pointcloud_camera_table)
-        self.t_pointcloud_camera_table = nn.Parameter(t_pointcloud_camera_table)
-        """
         self.input_q_camera_pointcloud_table, self.input_t_camera_pointcloud_table = inverse_SE3_qt_torch(
             q=q_pointcloud_camera_table, t=t_pointcloud_camera_table)
         self.q_camera_pointcloud_table = nn.Parameter(
@@ -34,11 +23,6 @@ class CameraPoses(nn.Module):
             self.input_t_camera_pointcloud_table)
 
     def forward(self, camera_pose_indices, reverse=False):
-        """
-        q_pointcloud_camera = self.q_pointcloud_camera_table[camera_pose_indices].contiguous()
-        t_pointcloud_camera = self.t_pointcloud_camera_table[camera_pose_indices].contiguous()
-        return q_pointcloud_camera, t_pointcloud_camera
-        """
         q_camera_pointcloud = self.q_camera_pointcloud_table[camera_pose_indices].contiguous(
         )
         t_camera_pointcloud = self.t_camera_pointcloud_table[camera_pose_indices].contiguous(
@@ -47,10 +31,6 @@ class CameraPoses(nn.Module):
 
     def normalize_quaternion(self):
         with torch.no_grad():
-            """
-            self.q_pointcloud_camera_table /= torch.norm(
-                self.q_pointcloud_camera_table, dim=-1, keepdim=True)
-            """
             self.q_camera_pointcloud_table /= torch.norm(
                 self.q_camera_pointcloud_table, dim=-1, keepdim=True)
 
